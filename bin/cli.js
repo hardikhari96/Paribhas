997dc5f37e5dcebad553f93f3249ec187d4e0a17
Harikrushna <32766262+hardikhari96@users.noreply.github.com>
2022-04-06 22:34:23 +0530

    cli error handle

--- bin/cli.js
+++ bin/cli.js
@@ -11,17 +11,22 @@ const runCommand = command => {
     }
     return true;
 }
-const repoName = process.env[2];
-const gitCheckoutCmd = `git clone --depth 1 https://github.com/hardikhari96/Paribhas.git ${repoName}`;
-const installDepsCommand = `cd ${repoName} && npm install`;
+try {
+    const repoName = process.env[2];
+    const gitCheckoutCmd = `git clone --depth 1 https://github.com/hardikhari96/Paribhas.git ${repoName}`;
+    const installDepsCommand = `cd ${repoName} && npm install`;
+    
+    console.log(`Creating project ${repoName}`);
+    const checkedOut = runCommand(gitCheckoutCmd);
+    if (!checkedOut) process.exit(-1);
+    
+    console.log(`Installing dependencies for ${repoName}`);
+    const installedDeps = runCommand(installDepsCommand);
+    if (!installedDeps); process.exit(-1)
+} catch (error) {
+    console.log(error, "hari")
+}
 
-console.log(`Creating project ${repoName}`);
-const checkedOut = runCommand(gitCheckoutCmd);
-if (!checkedOut) process.exit(-1);
-
-console.log(`Installing dependencies for ${repoName}`);
-const installedDeps = runCommand(installDepsCommand);
-if (!installedDeps); process.exit(-1)
 
 console.log("Congratulations! You are Ready. Follow the following command to start");
 console.log(`cd ${repoName} && npm start`)
