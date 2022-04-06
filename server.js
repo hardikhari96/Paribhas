79b7fa00a86dc86a9bb42bae9fcadf6a89b4b487
Harikrushna <32766262+hardikhari96@users.noreply.github.com>
2022-04-06 22:15:30 +0530

    new structure updated

--- server.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const port = process.env.PORT;
-const app = require('./routes/index');
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
